9bae6395bab7dcab67128d3677bdc19cf66659c2
James Kakos <jameskakos491@gmail.com>
2024-08-19 21:27:28 -0700

    Set CVars only when entering the world

--- KUtils/uiSettings.lua
+++ KUtils/uiSettings.lua
@@ -1,2 +1,15 @@
-SetCVar("raidOptionDisplayMainTankAndAssist", 0)
-SetCVar("autoLootDefault", 1)
+local function loadCVars()
+	SetCVar("raidOptionDisplayMainTankAndAssist", 0)
+	SetCVar("autoLootDefault", 1)
+end
+
+local CVarLoader = CreateFrame("Frame")
+CVarLoader:RegisterEvent("PLAYER_ENTERING_WORLD")
+CVarLoader:SetScript(
+	"OnEvent", 
+	function(self, event)
+		if event == "PLAYER_ENTERING_WORLD" then
+			loadCVars()
+		end
+	end
+)
